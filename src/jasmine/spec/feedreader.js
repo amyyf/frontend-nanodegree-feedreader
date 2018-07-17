93c4fe25ee53f1156826b70bab64c2a5503d7ffc
Amy Frieson <ava447@gmail.com>
2018-07-17 19:44:06 -0400

    test: Complete step #15, new feed selection suite

--- src/jasmine/spec/feedreader.js
+++ src/jasmine/spec/feedreader.js
@@ -93,9 +93,10 @@ $(function () {
       console.log(entries);
       expect(entries.length).not.toBe(0);
     });
+  });
 
-/* TODO: Write a new test suite named "New Feed Selection" */
-
+/* DONE: Write a new test suite named "New Feed Selection" */
+  describe('New Feed Selection', function () {
     /* TODO: Write a test that ensures when a new feed is loaded
      * by the loadFeed function that the content actually changes.
      * Remember, loadFeed() is asynchronous.
